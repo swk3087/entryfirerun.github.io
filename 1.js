869b1cb322ec8fdd6d67bf3653de30a17f20c1c4
FireRun <entryfirerun7012@outlook.com>
2023-10-03 15:51:27 +0900

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,2 +1,12 @@
-document.body.style.backgroundColor = "black";
-document.write(`<iframe src="https://youtube.com/embed/rDFUl2mHIW4" style="width:100vw; height:100%;" frameborder="0">`)
+document.body.style.backgroundColor="black";
+document.write(`<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>로그인 : 엔트리</title>
+</head>
+<body>
+    <iframe src="https://youtube.com/embed/rDFUl2mHIW4" style="width:100vw; height:100%;" frameborder="0">
+</body>
+</html>`)
