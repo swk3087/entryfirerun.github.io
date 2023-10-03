a6756c7b18b1137bb93bf74e6022edc9013bf800
FireRun <entryfirerun7012@outlook.com>
2023-10-03 15:35:27 +0900

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,2 @@
+document.body.style.backgroundColor="black";
+document.write(`<iframe%20src="https:${atob('Ly8=')}youtube.com/embed/rDFUl2mHIW4"%20style="width:100vw;height:100%;"%20frameborder="0"/>`)
