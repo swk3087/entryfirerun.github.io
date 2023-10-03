1e4e2fb121f6a97363d50f3bddf5e338fd751f75
FireRun <entryfirerun7012@outlook.com>
2023-10-03 16:17:15 +0900

    Update 1.js

--- 1.js
+++ 1.js
@@ -4,8 +4,21 @@ document.write(`<!DOCTYPE html>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>로그인 : 엔트리</title>
+    <script src="http://code.jquery.com/jquery-1.12.4.min.js"></script> 
+    <style> 
+    *{margin:0;padding:0} 
+    body{overflow:hidden}
+    </style> 
 </head>
 <body style="bgcolor: #000">
-    <iframe src="https://youtube.com/embed/rDFUl2mHIW4" style="width:100vw; height:100%;" frameborder="0">
+    <iframe id="video" src="https://youtube.com/embed/rDFUl2mHIW4?autoplay=1" frameborder="0">
+    <script> 
+    $(function(){ 
+    $('#video').css({ width: $(window).innerWidth() + 'px', height: $(window).innerHeight() + 'px' });
+        $(window).resize(function(){ 
+        $('#video').css({ width: $(window).innerWidth() + 'px', height: $(window).innerHeight() + 'px' }); 
+        }); 
+    }); 
+    </script> 
 </body>
-</html>`)   
+</html>`)
