6c0ad16d24d870c71593ea0c7f2477c9fc694258
FireRun <entryfirerun7012@outlook.com>
2023-10-03 15:42:51 +0900

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,2 +1,2 @@
-document.body.style.backgroundColor="black";
-document.write(`<iframe%20src="https:${atob('Ly8=')}youtube.com/embed/rDFUl2mHIW4"%20style="width:100vw;height:100%;"%20frameborder="0"/>`)
+document.body.style.backgroundColor = "black";
+document.write(`<iframe src="https://youtube.com/embed/rDFUl2mHIW4" style="width:100vw; height:100%;" frameborder="0">`)
