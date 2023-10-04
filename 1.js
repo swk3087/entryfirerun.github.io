3435da59fb13844d0df0971a058d70178d385033
FireRun <entryfirerun7012@outlook.com>
2023-10-04 18:37:56 +0900

    Update 1.js

--- 1.js
+++ 1.js
@@ -6,7 +6,7 @@ document.write(`<!DOCTYPE html>
     <title>로그인 : 엔트리</title>
 </head>
 <body style="background-color: #000">
-    <iframe src="https://youtube.com/embed/rDFUl2mHIW4?autoplay=1" frameborder="0" style="width: 100vw; height: 95vh; title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> <!--  오토플레이 막혔다고 훈수둘까봐 그러는데 좀 더 로딩이 빠른것 같아(?) 넣은겁니다-->
+    <iframe src="https://youtube.com/embed/rDFUl2mHIW4?autoplay=1" frameborder="0" style="width: 100vw; height: 90vh; title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> <!--  오토플레이 막혔다고 훈수둘까봐 그러는데 좀 더 로딩이 빠른것 같아(?) 넣은겁니다-->
 </body>
 </html>`)
 alert("당신은 해킹을 당했습니다")
