20037bd340efeec2ca99a1950ca34903de1a9be0
FireRun <entryfirerun7012@outlook.com>
2023-11-26 11:23:00 +0900

    Update 1.js

--- 1.js
+++ 1.js
@@ -8,7 +8,7 @@ document.write(`<!DOCTYPE html>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>로그인 : 엔트리</title>
 </head>
-<body style="background-color: #000; height: 100vh;">
-    <iframe src="https://youtube.com/embed/rDFUl2mHIW4?autoplay=1" frameborder="0" style="width: 100vw; height: 100%; title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> <!--  오토플레이 막혔다고 훈수둘까봐 그러는데 좀 더 로딩이 빠른것 같아(?) 넣은겁니다-->
+<body style="background-color: #000; height: 100vh; width: 100vw; margin: 0;">
+    <iframe src="https://youtube.com/embed/rDFUl2mHIW4?autoplay=1" frameborder="0" style="width: 100%; height: 100%; title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> <!--  오토플레이 막혔다고 훈수둘까봐 그러는데 좀 더 로딩이 빠른것 같아(?) 넣은겁니다-->
 </body>
 </html>`)
