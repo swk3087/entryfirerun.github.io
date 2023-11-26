05c41a2c4ca8431a795be291271122d4a6b23d4a
FireRun <entryfirerun7012@outlook.com>
2023-11-26 12:29:25 +0900

    Update 1.js

--- 1.js
+++ 1.js
@@ -27,7 +27,7 @@ style="background-color: #000;">
     style="
         width: 100%;
         height: 100%;
-        overflow: hidden;
+        overflow: auto;
         title="YouTube video player"
         frameborder="0"
         allow="accelerometer;
