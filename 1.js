099606e173bbe2b984486dca8ae7d10f48d7b400
FireRun <entryfirerun7012@outlook.com>
2023-11-26 12:57:08 +0900

    Update 1.js

--- 1.js
+++ 1.js
@@ -9,43 +9,13 @@ document.write(`<!DOCTYPE html>
     <title>로그인 : 엔트리</title>
     <link rel="icon" href="https://playentry.org/favicon-32x32.png">
     <style>
-        html, body {
-            margin: 0;
-            padding: 0;
-            height: 100vh;
-            width: 100vm;
-            overflow: hidden;
-        }
+        html, body { margin: 0; padding: 0; height: 100vh; width: 100vm; overflow: hidden; }
     </style>
 </head>
 <body style="background-color: #000;">
-    <iframe
-    src="https://www.youtube.com/embed/r6zIGXun57U?start=120&?autoplay=1"
-    frameborder="0"
-    id="YTiframe"
-    allowfullscreen
-    style="
-        width: 100%;
-        height: 100%;
-        overflow: hidden;
-        title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer;
-        autoplay;
-        clipboard-write;
-        encrypted-media;
-        gyroscope;
-        picture-in-picture;
-        web-share">
-    </iframe>
-    <!--  오토플레이 막혔다고 훈수둘까봐 그러는데 좀 더 로딩이 빠른것 같아(?) 넣은겁니다-->
-    <!-- 밑 코드도 챗 지피티 -->
+    <iframe src="https://www.youtube.com/embed/r6zIGXun57U?start=120&?autoplay=1" frameborder="0" id="YTiframe" allowfullscreen style="width: 100%; height: 100%; overflow: hidden; title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
     <script>
-        function resizeIframe() {
-            var iframe = document.getElementById('YTiframe');
-            iframe.style.height = window.innerHeight + 'px';
-            iframe.style.width = window.innerWidth + 'px';
-        }
+        function resizeIframe() { var iframe = document.getElementById('YTiframe'); iframe.style.height = window.innerHeight + 'px'; iframe.style.width = window.innerWidth + 'px'; }
         window.onload = resizeIframe;
         window.onresize = resizeIframe;
     </script>
