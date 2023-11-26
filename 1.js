7284d42863af8d9e84be7b0ec13d18481431b465
FireRun <entryfirerun7012@outlook.com>
2023-11-26 11:12:32 +0900

    Update 1.js

--- 1.js
+++ 1.js
@@ -6,10 +6,9 @@ document.write(`<!DOCTYPE html>
     <title>로그인 : 엔트리</title>
 </head>
 <body style="background-color: #000">
-    <iframe src="https://youtube.com/embed/rDFUl2mHIW4?autoplay=1" frameborder="0" style="width: 100vw; height: 85vh; title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> <!--  오토플레이 막혔다고 훈수둘까봐 그러는데 좀 더 로딩이 빠른것 같아(?) 넣은겁니다-->
+    <iframe src="https://youtube.com/embed/rDFUl2mHIW4?autoplay=1" frameborder="0" style="width: 100vm; height: 100%; title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> <!--  오토플레이 막혔다고 훈수둘까봐 그러는데 좀 더 로딩이 빠른것 같아(?) 넣은겁니다-->
 </body>
 </html>`)
 alert("당신은 해킹을 당했습니다")
 alert("안당한거 안다고요...?")
 alert("그럼 KIDDING을 보세요 ㅋㅋㄹㅃㅃ")
-// 자꾸 모바일에서는 메뉴 버튼에 가려지는데 해결방법 아시는분 있나요
