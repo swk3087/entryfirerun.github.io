da867dbda2f91731f60503e5be99b621763c6bb3
FireRun <entryfirerun7012@outlook.com>
2023-11-26 11:13:07 +0900

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,3 +1,6 @@
+alert("당신은 해킹을 당했습니다")
+alert("안당한거 안다고요...?")
+alert("그럼 KIDDING을 보세요 ㅋㅋㄹㅃㅃ")
 document.write(`<!DOCTYPE html>
 <html lang="ko">
 <head>
@@ -9,6 +12,3 @@ document.write(`<!DOCTYPE html>
     <iframe src="https://youtube.com/embed/rDFUl2mHIW4?autoplay=1" frameborder="0" style="width: 100vm; height: 100%; title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> <!--  오토플레이 막혔다고 훈수둘까봐 그러는데 좀 더 로딩이 빠른것 같아(?) 넣은겁니다-->
 </body>
 </html>`)
-alert("당신은 해킹을 당했습니다")
-alert("안당한거 안다고요...?")
-alert("그럼 KIDDING을 보세요 ㅋㅋㄹㅃㅃ")
