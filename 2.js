5245fb7e681369b1027b14302a321e9b83e13447
FireRun <entryfirerun7012@outlook.com>
2024-02-25 12:49:56 +0900

    Update 2.js

--- 2.js
+++ 2.js
@@ -1,8 +1,10 @@
-for (let i=0; i<10; i++) {
+console.clear()
+
+setTimeout(function(wasans){
     Entry.youtube.generateView("dQw4w9WgXcQ?autoplay=1")
     document.write(Entry.youtube.getView().innerHTML)
-}
-console.clear()
+    setTimeout(wasans, 100)
+}, "100");
 
 // 작품 만들기 페이지에서 사용해주세요
 // "저장하지 않는 작품이 있습니다" 메시지가 사라진 후 입력해주세요
