2deb6e03fcaaa37654813857a328f698306af625
FireRun <entryfirerun7012@outlook.com>
2024-04-27 14:07:30 +0900

    Update 5.js

--- 5.js
+++ 5.js
@@ -9,6 +9,22 @@ const index = body.indexOf("csrf-token") + 21;
 const csrfToken = body.slice(index, index + 36);
 const xToken = body.split('xToken":"')[1].split('"')[0]
 
+const projectlink = "662c5d4a86b607b92acaa8c6" // 여기에 작품 아이디 입력이라고 적혀 있는 부분 수정해주세요 ( 예 : const = "662375378f43d570133c2f23" )
+fetch("https://playentry.org/graphql/", {
+  headers: {
+    "content-type": "application/json",
+    "csrf-token": `${csrfToken}`,
+    "x-client-type": "Client",
+    "x-token": `${xToken}`,
+  },
+  referrerPolicy: "unsafe-url",
+  body: `{"query":"mutation LIKE($target: String, $targetSubject: String){like(target: $target, targetSubject: $targetSubject){target}}","variables":{"target":"${projectlink}","targetSubject":"project"}}`,
+
+  method: "POST",
+  mode: "cors",
+  credentials: "include",
+});
+  
 const helpmoongu = Math.random() * 3
 if (helpmoongu == 3) {
   helpmoongu = 2
