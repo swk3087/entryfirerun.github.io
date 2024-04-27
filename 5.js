7a1d28d29a7f3d0134680b660e1090c6226bb9a7
FireRun <entryfirerun7012@outlook.com>
2024-04-27 12:05:03 +0900

    Update 5.js

--- 5.js
+++ 5.js
@@ -1,15 +1,20 @@
+let csrf;
+let body;
+async function getCookie ()  {
+  csrf = await fetch("https://playentry.org");
+  body = await csrf.text();
+}
+getCookie().then(()=>{
+const index = body.indexOf("csrf-token") + 21;
+const csrfToken = body.slice(index, index + 36);
+const xToken = body.split('xToken":"')[1].split('"')[0]
+    
 fetch("https://playentry.org/graphql", {
   headers: {
     "content-type": "application/json",
-    "csrf-token": `${
-      JSON.parse(document.getElementById("__NEXT_DATA__").innerText).props
-        .initialProps.csrfToken
-    }`,
+    "csrf-token": `${csrfToken}`,
     "x-client-type": "Client",
-    "x-token": `${
-      JSON.parse(document.getElementById("__NEXT_DATA__").innerText).props
-        .initialState.common.user.xToken
-    }`,
+    "x-token": `${xToken}`,
   },
   referrerPolicy: "unsafe-url",
   body: `{"query":"mutation LIKE($target: String, $targetSubject: String){like(target: $target, targetSubject: $targetSubject){target}}","variables":{"target":"66226c711d03bdd15aa60f7c","targetSubject":"discuss"}}`,
@@ -18,3 +23,4 @@ fetch("https://playentry.org/graphql", {
   mode: "cors",
   credentials: "include",
 });
+})
