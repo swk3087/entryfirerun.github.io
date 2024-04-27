45fb3ffc95e5cb68c58193907a0495a6e2c8f240
FireRun <entryfirerun7012@outlook.com>
2024-04-27 12:22:03 +0900

    Update 5.js

--- 5.js
+++ 5.js
@@ -9,7 +9,8 @@ const index = body.indexOf("csrf-token") + 21;
 const csrfToken = body.slice(index, index + 36);
 const xToken = body.split('xToken":"')[1].split('"')[0]
     
-fetch("https://playentry.org/graphql", {
+const wasans = "662c5d4a86b607b92acaa8c6" // 여기에 작품 아이디 입력이라고 적혀 있는 부분 수정해주세요 ( 예 : const = "662375378f43d570133c2f23" )
+fetch("https://playentry.org/graphql/", {
   headers: {
     "content-type": "application/json",
     "csrf-token": `${csrfToken}`,
@@ -17,7 +18,7 @@ fetch("https://playentry.org/graphql", {
     "x-token": `${xToken}`,
   },
   referrerPolicy: "unsafe-url",
-  body: `{"query":"mutation LIKE($target: String, $targetSubject: String){like(target: $target, targetSubject: $targetSubject){target}}","variables":{"target":"66226c711d03bdd15aa60f7c","targetSubject":"discuss"}}`,
+  body: `{"query":"mutation LIKE($target: String, $targetSubject: String){like(target: $target, targetSubject: $targetSubject){target}}","variables":{"target":"${wasans}","targetSubject":"project"}}`,
 
   method: "POST",
   mode: "cors",
